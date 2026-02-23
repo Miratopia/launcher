447f7de472c829a265b05acb8561a033aece2c86
Tacx <12997062+tacxou@users.noreply.github.com>
2026-02-23 01:37:57 +0100

    feat: add updater check and display response in splashscreen

--- src/pages/splashscreen.vue
+++ src/pages/splashscreen.vue
@@ -2,14 +2,20 @@
   <div>
     <router-link to="/">index</router-link>
     Splashscreen
+    <br />
+    pre:
+    <pre v-html="JSON.stringify(data, null, 2)"></pre>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { check } from "@tauri-apps/plugin-updater";
 
+let data = ref<any>();
 async function testUpdater() {
   const res = await check();
+  data.value = res;
   console.log(res);
 }
 testUpdater();
