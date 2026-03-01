3935970de2c4c4803d81d05284377af4cffa74df
Samsaoule <samuel034bt@gmail.com>
2026-03-01 19:27:19 +0100

    :clean: clean server status

--- src/components/ServerStatus.vue
+++ src/components/ServerStatus.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-import { useAccountsCommand } from '../composables/useAccountsCommand';
-
-const { displayActiveAccount } = useAccountsCommand();
-
-const activeAccount = await displayActiveAccount();
-console.log('active account', activeAccount);
 </script>
 
 <template>
   <div class="flex-1 flex items-center justify-center">
-    <div class="text-start">
+    <div class="text-center">
       <img
         src="/full-logo.webp"
         alt="Miratopia"
         class="h-20 object-contain mx-auto"
       />
-      <pre class="w-64">{{ activeAccount }}</pre>
       <p class="text-amber-300/30 text-sm mt-3 tracking-widest uppercase">
         Serveur Minecraft Communautaire
       </p>
